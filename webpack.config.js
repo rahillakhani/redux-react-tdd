d12371396aa7c459f5ad0b2f5f02ca55088744ce
Rahil Lakhani <lakhani.rj@gmail.com>
2017-03-30 21:42:11 +0530

    added basic react setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const output = path.resolve(__dirname, 'public');
+const dev = path.resolve(__dirname, 'app');
+// const server = path.resolve(__dirname, 'server');
+
+const clientConfig = {
+	target: 'web',
+	entry: `${dev}/index.js`,
+	output: {
+		path: output,
+		filename: 'js/bundle.js',
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx'],
+	},
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			exclude: 'node_modules',
+			include: dev,
+			loader: 'babel',
+		}],
+	},
+};
+
+module.exports = [clientConfig];
+
